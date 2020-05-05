a7ac146ba0c7d3649bd79657710376b0bf9ee0f1
Evelyn Farias <evyufcg@gmail.com>
2020-05-05 15:15:08 -0300

    Update firebase.js

--- webeventpublisher/src/config/firebase.js
+++ webeventpublisher/src/config/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAXDJFMOJ1ouLz9JZgLLOm5EI8xvqM_czo",
+    apiKey: "apikey",
     authDomain: "webeventpublisher.firebaseapp.com",
     databaseURL: "https://webeventpublisher.firebaseio.com",
     projectId: "webeventpublisher",
     storageBucket: "webeventpublisher.appspot.com",
-    messagingSenderId: "210675909413",
-    appId: "1:210675909413:web:cf51fe69bab58ca7d490cc"
+    messagingSenderId: "senderId",
+    appId: "appId"
   };
   // Initialize Firebase
   export default firebase.initializeApp(firebaseConfig);
